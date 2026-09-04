0c673caa9efe0189fa92709f0139010866875bc2
darren <darrentoh50@gmail.com>
2026-09-04 21:45:41 +0800

    Update code based on class instructions

---
+++ src/main/java/circus/animal/Animal.java
@@ -0,0 +1,16 @@
+package circus.animal;
+
+import circus.Asset;
+
+public abstract class Animal implements Asset {
+
+    public abstract String speak();
+
+//    public int getValue(){
+//        return 5;
+//    }
+}
+
+
+// Object class
+// Object o = new Animal();
